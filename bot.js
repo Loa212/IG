2fe97adf94ba0c504a3c92986f5b0a87f989078b
Loris Anobile <anobileloris@gmail.com>
2020-03-11 17:12:08 +0100

    first commit

---
+++ bot.js
@@ -0,0 +1,32 @@
+var following = [];
+var followers = [];
+
+
+function instaBot(cicli, numero){
+
+    for (i = 0; i < cicli; i++) {
+        text += "The number is " + i + "<br>";
+      }
+    return(console.log(`u followed ${nFollows} accounts`));
+}
+
+
+
+
+//Main
+
+setInterval(function() {
+    var Heart = document.querySelector('button.wpO6b');
+    var Arrow = document.querySelector('a._65Bje.coreSpriteRightPaginationArrow');
+    
+if(Heart) {
+Heart.click();
+}
+
+setTimeout(function(){
+    Arrow.click(); //do what you need here
+}, 2000);
+
+}, 3000);
+
+// endMain
